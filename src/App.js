804933603d1d99965d84926f4e0b7e95dd1ef8bb
sabrinaaraujoo <cien.sabrinaaraujo@outlook.com>
2023-01-16 23:30:53 -0300

    Navbar e Footer

--- src/App.js
+++ src/App.js
@@ -6,16 +6,22 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 
+// components
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <div className="container">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
           </Routes>
         </div>
+        <Footer />
       </BrowserRouter>
     </div>
   );
