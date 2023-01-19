19fef7d1100ef9162533f170fe0d237a7dfc6df7
sabrinaaraujoo <cien.sabrinaaraujo@outlook.com>
2023-01-19 08:39:50 -0300

    Contexto da autenticação

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
+//context
+import { AuthProvider } from "./context/AuthContext";
+
 //pages
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
@@ -15,18 +18,20 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
