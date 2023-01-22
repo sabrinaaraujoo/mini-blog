8b8b2bdf47653b2e27f098c221f8b1fa48022e34
sabrinaaraujoo <cien.sabrinaaraujo@outlook.com>
2023-01-21 23:25:25 -0300

    função Logout

--- src/hooks/useAuthentication.jsx
+++ src/hooks/useAuthentication.jsx
@@ -4,7 +4,7 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   updateProfile,
-  singOut,
+  signOut,
 } from "firebase/auth";
 
 import { useState, useEffect } from "react";
@@ -25,6 +25,7 @@ export const useAuthentication = () => {
     }
   }
 
+  // Register
   const createUser = async (data) => {
     checkIfIsCancelled();
 
@@ -63,6 +64,13 @@ export const useAuthentication = () => {
     console.log(error);
   };
 
+  // logout  - sing out
+  const logout = () => {
+    checkIfIsCancelled();
+
+    signOut(auth);
+  };
+
   useEffect(() => {
     return () => setCancelled(true);
   }, []);
@@ -72,5 +80,6 @@ export const useAuthentication = () => {
     createUser,
     error,
     loading,
+    logout,
   };
 };
