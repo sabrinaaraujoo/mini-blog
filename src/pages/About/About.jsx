553c46827d4e7c016082d0bdf789377259a866a9
sabrinaaraujoo <cien.sabrinaaraujo@outlook.com>
2023-01-21 23:09:54 -0300

    Página Sobre

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 // CSS
 import styles from "./About.module.css";
 
 const About = () => {
-  return <h1>About</h1>;
+  return (
+    <div className={styles.about}>
+      <h2>
+        Sobre o Mini <span>Blog</span>
+      </h2>
+      <p>
+        Este projeto consiste em um blog feito com React no Fron-End e Firebase
+        no Back-End
+      </p>
+      <Link to="/posts/create" className="btn">
+        Criar Post
+      </Link>
+    </div>
+  );
 };
 
 export default About;
