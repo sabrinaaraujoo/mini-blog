4adf81e3876fcf36a0f6dc7b4d19853300d60de3
sabrinaaraujoo <cien.sabrinaaraujo@outlook.com>
2023-01-22 22:24:15 -0300

    Validação dos dados

--- src/pages/CreatePost/CreatePost.jsx
+++ src/pages/CreatePost/CreatePost.jsx
@@ -16,15 +16,30 @@ const CreatePost = () => {
 
   const { insertDocument, response } = useInsertDocument("posts");
 
+  const navigate = useNavigate();
+
   const handleSubmit = (e) => {
     e.preventDefault();
     setFormError("");
 
     // validate image URL
+    try {
+      new URL(image);
+    } catch (error) {
+      setFormError("A imagem precisa ser uma URL");
+    }
 
     // array tags
 
+    const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase());
+
     //values
+
+    if (!title || !image || !tags || !body) {
+      setFormError("Por favor, preencha todos os campos!");
+    }
+
+    if (formError) return;
 
     // console.log({
     //   title,
@@ -39,12 +54,13 @@ const CreatePost = () => {
       title,
       image,
       body,
-      tags,
+      tagsArray,
       uid: user.uid,
       createdBy: user.displayName,
     });
 
     // redirect to home page
+    navigate("/");
   };
 
   return (
