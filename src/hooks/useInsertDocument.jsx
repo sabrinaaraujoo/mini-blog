07df4cfd464be551c6ff179753fd6f778cc31b6d
sabrinaaraujoo <cien.sabrinaaraujo@outlook.com>
2023-01-22 22:03:18 -0300

    Inserindo dados no firebase

--- src/hooks/useInsertDocument.jsx
+++ src/hooks/useInsertDocument.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect, useReducer } from "react";
 import { db } from "../firebase/config";
 import { collection, addDoc, Timestamp } from "firebase/firestore";
-import { async } from "@firebase/util";
 
 const initialState = {
   loading: null,
