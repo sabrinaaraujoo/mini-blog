521e334827d5e1bde37d2fb8bfdac31701b4514a
sabrinaaraujoo <cien.sabrinaaraujo@outlook.com>
2023-01-23 20:32:38 -0300

    Hook de resgate de dados

---
+++ src/hooks/useFetchDocuments.jsx
@@ -0,0 +1,63 @@
+import { useState, useEffect } from "react";
+import { db } from "../firebase/config";
+import {
+  collection,
+  query,
+  ordeBy,
+  onSnpaoshot,
+  where,
+  snapshotEqual,
+  QuerySnapshot,
+} from "firebase/firestore";
+import { async } from "@firebase/util";
+
+export const useFetchDocuments = (docCollection, search = null, uid = null) => {
+  const [documents, setDocuments] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  // dal with memory leak
+  const [cancelled, setCancelled] = useState(false);
+
+  useEffect(() => {
+    async function loadData() {
+      if (cancelled) return;
+
+      setLoading(true);
+
+      const collectionRef = await collection(db, docCollection);
+
+      try {
+        let q;
+
+        // busca
+        // dashborad
+
+        q = await query(collectionRef, ordeBy("createdAt", "desc"));
+        await onSnpaoshot(q, (querySnapshot) => {
+          setDocuments(
+            querySnapshot.docs.map((doc) => ({
+              id: doc.id,
+              ...doc.data(),
+            }))
+          );
+        });
+
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        setError(error.message);
+
+        setLoading(false);
+      }
+    }
+
+    loadData();
+  }, [docCollection, search, uid, cancelled]);
+
+  useEffect(() => {
+    return () => setCancelled(true);
+  }, []);
+
+  return { documents, loading, error };
+};
