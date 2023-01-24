d9c9c41a45d2fd032a24b3c3b19cdbeeafd65ecf
sabrinaaraujoo <cien.sabrinaaraujo@outlook.com>
2023-01-24 20:52:17 -0300

    CSS da página de post individual

--- src/pages/Posts/Post.jsx
+++ src/pages/Posts/Post.jsx
@@ -3,13 +3,29 @@ import styles from "./Post.module.css";
 
 // hooks
 import { useParams } from "react-router-dom";
+import { useFetchDocument } from "../../hooks/useFetchDocument";
 
 const Post = () => {
   const { id } = useParams();
+  const { document: post, loading } = useFetchDocument("posts", id);
 
   return (
-    <div>
-      <h1>Post {id}</h1>
+    <div className={styles.post_container}>
+      {loading && <p>Carregando post...</p>}
+      {post && (
+        <>
+          <h1>{post.title}</h1>
+          <img src={post.image} alt={post.title} />
+          <p>{post.body}</p>
+          <h3>Este post se trata sobre:</h3>
+          {post.tagsArray.map((tag) => (
+            <p className={styles.tags} key={tag.id}>
+              <span>#</span>
+              {tag}
+            </p>
+          ))}
+        </>
+      )}
     </div>
   );
 };
