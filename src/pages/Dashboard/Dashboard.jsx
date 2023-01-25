cf4306300e5e6ef66bfecd4e3412ee3969f94ded
sabrinaaraujoo <cien.sabrinaaraujo@outlook.com>
2023-01-24 21:03:01 -0300

    Estruturando dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 import styles from "./Dashboard.module.css";
 
+import { Link } from "react-router-dom";
+
+// hooks
+import { useAuthValue } from "../../context/AuthContext";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+
 const Dashboard = () => {
+  const { user } = useAuthValue();
+
+  const uid = user.id;
+
+  //post do usuario
+  const posts = [];
+
   return (
     <div>
-      <h1>Dashboard</h1>
+      <h2>Dashboard</h2>
+      <p>Gerencie os seus posts</p>
+      {posts && posts.length === 0 ? (
+        <div className={styles.noposts}>
+          <p>Não foram encontrados posts</p>
+          <Link to="/posts/create" className="btn">
+            Criar primeiro post
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <p>Tem posts!</p>
+        </div>
+      )}
     </div>
   );
 };
