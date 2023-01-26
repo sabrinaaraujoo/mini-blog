e935b86646cd11a222fffe8c3d7e8ed63cce4bc8
sabrinaaraujoo <cien.sabrinaaraujo@outlook.com>
2023-01-25 22:11:12 -0300

    Atualizando post

--- src/pages/EditPost/EditPost.jsx
+++ src/pages/EditPost/EditPost.jsx
@@ -3,7 +3,7 @@ import styles from "./EditPost.module.css";
 import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useAuthValue } from "../../context/AuthContext";
-import { useInsertDocument } from "../../hooks/useInsertDocument";
+import { useUpdateDocument } from "../../hooks/useUpdateDocument";
 import { useFetchDocument } from "../../hooks/useFetchDocument";
 
 const EditPost = () => {
@@ -29,7 +29,7 @@ const EditPost = () => {
 
   const { user } = useAuthValue();
 
-  const { insertDocument, response } = useInsertDocument("posts");
+  const { updateDocument, response } = useUpdateDocument("posts");
 
   const navigate = useNavigate();
 
@@ -56,26 +56,19 @@ const EditPost = () => {
 
     if (formError) return;
 
-    // console.log({
-    //   title,
-    //   image,
-    //   body,
-    //   tags,
-    //   uid: user.uid,
-    //   createdBy: user.displayName,
-    // });
-
-    insertDocument({
+    const data = {
       title,
       image,
       body,
       tagsArray,
       uid: user.uid,
       createdBy: user.displayName,
-    });
+    };
+
+    updateDocument(id, data);
 
     // redirect to home page
-    navigate("/");
+    navigate("/dashboard");
   };
 
   return (
